34d827cb3ee7fe84559b93c693b19f5a3ef21666
siddhantdrk <siddhantdrk@gmail.com>
2023-01-20 12:17:12 +0530

    smtp risk removed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,8 +45,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            'siddhantdrk@gmail.com',
-    password:             'thcoaaukkgschocb',
+    user_name:            'username@gmail.com',
+    password:             'password',
     authentication:       'plain',
     enable_starttls_auto: true  }
 
